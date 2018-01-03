f0257a5ca1a79f45d78851011606bbf10082ec17
Andrew Maul <andymaul123@gmail.com>
2018-01-02 22:44:15 -0700

    Adds some initial steps. Still mostly exploratory.

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-const opn = require('opn');
+const   opn = require('opn'),
+        fse = require('fs-extra'),
+        util = require('util'),
+        path = require('path'),
+        imgsize = util.promisify(require('image-size'));
 
-console.log("running");
-function launch() {
-	console.log('bananas');
-// fs.open('/Users/amaul/git-directory/moodboards', 'a+', (err, fd) => {
-//   // => [Error: EISDIR: illegal operation on a directory, open <directory>]
+let dir = './sample';
+
+// fse.readFile('./board.html', 'utf8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
 // });
 
-}
+//opn('D:/GitHub/moodboards/board.html',{wait:false});
 
-opn('/Users/amaul/git-directory/moodboards/board.html',{wait:false});
-console.log("finish");
-module.exports = launch;
+
+
+// Step 1: using fs.readDir, iterate over files (see https://stackoverflow.com/questions/32511789/looping-through-files-in-a-folder-node-js)
+fse.readdir( dir, function( err, files ) {
+        if( err ) {
+            console.error( "Could not list the directory.", err );
+            process.exit( 1 );
+        } 
+
+        files.forEach( function( file, index ) {
+            //Step 1.1 for each iterable, check file extensions against match pattern
+            console.log(fse.lstatSync(dir + '/' + file).isFile());
+            console.log(path.extname(dir + '/' + file));
+            // Step 1.2 if it's an image, get dimensions
+            imgsize(dir + '/' + file)
+                .then(dimensions => { console.log(dimensions.width, dimensions.height); })
+                .catch(err => console.error(err));
+        });
+} );
