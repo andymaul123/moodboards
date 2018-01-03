3a5030c0d9143055db76d87679c140ea7010cfa7
Andrew Maul <andrew.maul@meltmedia.com>
2018-01-03 15:54:52 -0700

    WIP

--- app.js
+++ app.js
@@ -5,15 +5,30 @@ const   opn = require('opn'),
         imgsize = util.promisify(require('image-size'));
 
 let dir = './sample';
+let imageExtensions = ['.jpg','.jpeg','.png'];
+let fileExtensions = ['.txt'];
 
 // fse.readFile('./board.html', 'utf8', (err, data) => {
 //   if (err) throw err;
 //   console.log(data);
 // });
 
+function handleImages(image) {
+    imgsize(image)
+        .then(dimensions => { 
+            //eventually return the dimensions to calculator method for weighting
+            console.log(dimensions.width, dimensions.height); 
+        })
+        .catch(err => {
+            console.error(err)
+        });
+}
+
+function handleTextFiles(text) {
+    console.log("Will handle text file from here");
+}
+
 //opn('D:/GitHub/moodboards/board.html',{wait:false});
-
-
 
 // Step 1: using fs.readDir, iterate over files (see https://stackoverflow.com/questions/32511789/looping-through-files-in-a-folder-node-js)
 fse.readdir( dir, function( err, files ) {
@@ -23,12 +38,11 @@ fse.readdir( dir, function( err, files ) {
         } 
 
         files.forEach( function( file, index ) {
-            //Step 1.1 for each iterable, check file extensions against match pattern
-            console.log(fse.lstatSync(dir + '/' + file).isFile());
-            console.log(path.extname(dir + '/' + file));
-            // Step 1.2 if it's an image, get dimensions
-            imgsize(dir + '/' + file)
-                .then(dimensions => { console.log(dimensions.width, dimensions.height); })
-                .catch(err => console.error(err));
+            //Step 1.1 for each iterable, check if it's a file and if it's an acceptable image type
+            if(fse.lstatSync(dir + '/' + file).isFile() && imageExtensions.indexOf(path.extname(dir + '/' + file)) > -1) {
+                handleImages(dir + '/' + file);
+            } else if(fse.lstatSync(dir + '/' + file).isFile() && fileExtensions.indexOf(path.extname(dir + '/' + file)) > -1) {
+                handleTextFiles(dir + '/' + file);
+            }
         });
 } );
